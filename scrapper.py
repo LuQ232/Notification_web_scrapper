bb38b230cae7d70a747cb8f2bc1f44b9e9026894
LuQ232 <piotrbednarek99@gmail.com>
2020-04-20 21:27:40 +0200

    Website scrapper functions done

---
+++ scrapper.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrapp_main_div():
+    url = "https://forum.pasja-informatyki.pl/questions/programowanie/c-plus-plus"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    div = str(soup.find(class_='qa-q-list qa-q-list-vote-disabled'))
+    return div.split('qa-q-list qa-q-list-vote-disabled">')[1]
+
+
+def list_of_last_posts():
+    div = scrapp_main_div()
+    list_of_last_posts_data= div.split('<!-- END qa-q-list-item -->')
+    return list_of_last_posts_data
+
+def title_of_post(post_data):
+    post_data=post_data.split('qa-q-item-title">')[1]
+    post_data=post_data.split('title=')[1]
+    post_data=post_data.split('</span>')[0]
+    post_data=post_data.split('">')[1]
+    return post_data
+
+def describtion_of_post(post_data):
+    post_data = post_data.split('qa-q-item-title">')[1]
+    post_data = post_data.split('title=')[1]
+    post_data = post_data.split('</span>')[0]
+    post_data = post_data.split('">')[0]
+    post_data = post_data.split('"')[1]
+    return post_data
+
+def time_of_post_exist(post_data):
+    post_data = post_data.split('qa-q-item-when-data">')[1]
+    post_data = post_data.split('</span>')[0]
+    return post_data
+
+def is_answear(post_data):
+    post_data = post_data.split('class="qa-a-count-data">')[1]
+    post_data = post_data.split('</span>')[0]
+    return post_data
+
+def number_of_views(post_data):
+    post_data = post_data.split('qa-view-count-data">')[1]
+    post_data = post_data.split('</span>')[0]
+    return post_data
+
+def author_of_post(post_data):
+    post_data = post_data.split('qa-q-item-who-data"')[1]
+    post_data = post_data.split('">')[1]
+    post_data = post_data.split('</a>')[0]
+    return post_data
+
+
+
+#print(list_of_last_posts()[0])
+#print(title_of_post(list_of_last_posts()[0]))
+#print(describtion_of_post(list_of_last_posts()[0]))
+#print(time_of_post_exist(list_of_last_posts()[0]))
+#print(is_answear(list_of_last_posts()[0]))
+#print(number_of_views(list_of_last_posts()[0]))
+#print(author_of_post(list_of_last_posts()[0]))
